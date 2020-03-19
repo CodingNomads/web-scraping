a97c43592c0073ec181f2bcc0279b7690a591707
martin-martin <breuss.martin@gmail.com>
2020-03-19 21:25:17 +0200

    Add labs

---
+++ labs/03_get_posts.py
@@ -0,0 +1,13 @@
+'''
+Create an account at freecycle
+
+Using python's request_html library:
+* log in to the website
+* navigate to the site that contains all posts for the Denver, CO group
+* retrieve all post titles from the first page
+* save the titles to a file called 'denver_posts.txt'
+
+BONUS: use pagination features to retrieve all posts of all pages in the group
+       and save them to the file
+
+'''
